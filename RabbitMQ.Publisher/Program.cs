fb2c74c6c23a575cb52f1b0611bfef06daf03363
hzeynep55 <83821699+hzeynep55@users.noreply.github.com>
2023-05-05 22:05:32 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hgiheins:k30j9gIcerohFeYDGwcPk6Z4jjjtgUwy@shrimp.rmq.cloudamqp.com/hgiheins");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    channel.QueueDeclare("Example", true, false, false);
+
+    string message = "Hello";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "Example", null, messageBody);
+    Console.WriteLine($"Gönderilen Mesaj: {message}");
+
+});
+
+
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hgiheins:k30j9gIcerohFeYDGwcPk6Z4jjjtgUwy@shrimp.rmq.cloudamqp.com/hgiheins");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("Example", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("Example",false,consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen mesaj: "+message);
+    channel.BasicAck(e.DeliveryTag,false);
+
+
+};
+Console.ReadLine();
